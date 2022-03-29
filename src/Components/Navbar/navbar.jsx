25d1bf43431cf11ff7b96f2101bf25830f5961d3
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2022-03-29 13:04:53 +0530

    minor Change

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -86,7 +86,7 @@ export default function Navbar() {
             aria-controls="navbarsExample04"
             aria-expanded="false"
             aria-label="Toggle navigation"
-            onClick={() => setfirst({ ...first, mMenu: !first.mMenu })}
+            onClick={() => setfirst({ mMenu: true })}
           >
             <span className="navbar-toggler-icon">
               <svg
@@ -122,7 +122,10 @@ export default function Navbar() {
           </button>
           <div className="collapse navbar-collapse" id="navbarsExample04">
             <ul className="navbar-nav me-auto mb-2 mb-md-0">
-              <li className="nav-item">
+              <li
+                className="nav-item"
+                onMouseEnter={() => setfirst({ pro: true })}
+              >
                 <a aria-current="page" href="#">
                   Products
                 </a>
@@ -133,7 +136,11 @@ export default function Navbar() {
               {/* <li class="nav-item">
           <a class="nav-link disabled">Disabled</a>
         </li> */}
-              <li className="nav-item dropdown">
+              <li
+                className="nav-item dropdown"
+                onMouseEnter={() => setfirst({ reso: true })}
+                onMouseLeave={() => setfirst({ reso: false })}
+              >
                 <a
                   href="#"
                   id="dropdown04"
@@ -142,115 +149,117 @@ export default function Navbar() {
                 >
                   Resources
                 </a>
-                <ul className="dropdown-menu" aria-labelledby="dropdown04">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      <svg
-                        width={24}
-                        height={24}
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M17.6725 6H5.32847C5.14733 6 5.00049 6.14684 5.00049 6.32798V18.672C5.00049 18.8532 5.14733 19 5.32847 19H17.6725C17.8536 19 18.0005 18.8532 18.0005 18.672V6.32798C18.0005 6.14684 17.8536 6 17.6725 6Z"
-                          stroke="#434F5A"
-                          strokeWidth="1.5"
-                          strokeMiterlimit={10}
-                        />
-                        <path
-                          d="M18.0003 13.0176H14.1638C13.9531 13.5591 13.5888 14.0273 13.1156 14.3645C12.6424 14.7018 12.0809 14.8934 11.5003 14.9158C10.9196 14.8934 10.3582 14.7018 9.88498 14.3645C9.41176 14.0273 9.04732 13.5591 8.83662 13.0176H5.00024"
-                          stroke="#434F5A"
-                          strokeWidth="1.5"
-                          strokeMiterlimit={10}
-                        />
-                      </svg>
-                      All Resources
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      <svg
-                        width={26}
-                        height={26}
-                        viewBox="0 0 26 26"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M18.8205 12.1708V20.5829H5.41675V5.25165H12.5253"
-                          stroke="#434F5A"
-                          strokeWidth="1.5"
-                          strokeLinejoin="round"
-                        />
-                        <path
-                          d="M8.32495 13.0613H15.712"
-                          stroke="#434F5A"
-                          strokeWidth="1.5"
-                          strokeMiterlimit={10}
-                        />
-                        <path
-                          d="M8.32495 16.4933H15.712"
-                          stroke="#434F5A"
-                          strokeWidth="1.5"
-                          strokeMiterlimit={10}
-                        />
-                        <path
-                          d="M8.32495 9.30566H12.4029"
-                          stroke="#434F5A"
-                          strokeWidth="1.5"
-                          strokeMiterlimit={10}
-                        />
-                        <path
-                          d="M17.9292 3.63545L18.4082 5.08368C18.442 5.1994 18.5124 5.30106 18.6088 5.37338C18.7053 5.44571 18.8225 5.48491 18.943 5.48491H20.4694C20.5917 5.48231 20.7116 5.51926 20.8112 5.59018C20.9108 5.6611 20.9849 5.76215 21.0225 5.87852C21.06 5.99489 21.059 6.1202 21.0196 6.23596C20.9802 6.35171 20.9045 6.45171 20.8038 6.52103L19.5782 7.41244C19.4793 7.4841 19.4055 7.58527 19.3675 7.70132C19.3295 7.81738 19.3291 7.94248 19.3664 8.05876L19.8344 9.50726C19.873 9.62223 19.8738 9.7465 19.8367 9.86197C19.7996 9.97745 19.7266 10.078 19.6282 10.149C19.5299 10.2199 19.4114 10.2576 19.2901 10.2564C19.1688 10.2552 19.0511 10.2151 18.9542 10.1422L17.7286 9.25075C17.6291 9.18074 17.5105 9.1433 17.3888 9.1433C17.2672 9.1433 17.1484 9.18074 17.0489 9.25075L15.8233 10.1422C15.7252 10.2118 15.6079 10.2492 15.4875 10.2491C15.3672 10.2489 15.2499 10.2114 15.1519 10.1416C15.0539 10.0718 14.9801 9.97321 14.9406 9.85953C14.9012 9.74584 14.8982 9.62275 14.9319 9.50726L15.4111 8.05876C15.4484 7.94248 15.448 7.81738 15.41 7.70132C15.372 7.58527 15.2982 7.4841 15.1993 7.41244L13.9626 6.52103C13.8682 6.4474 13.7989 6.34651 13.7643 6.23188C13.7297 6.11724 13.7313 5.99458 13.7692 5.88097C13.8071 5.76736 13.8792 5.66818 13.9757 5.59725C14.0721 5.52633 14.1883 5.48715 14.3081 5.48491H15.8233C15.9449 5.48379 16.0631 5.44455 16.1611 5.37257C16.259 5.30058 16.3319 5.19937 16.3693 5.08368L16.8372 3.63545C16.8772 3.52269 16.9513 3.4251 17.0491 3.35609C17.1468 3.28707 17.2636 3.25 17.3832 3.25C17.5029 3.25 17.6195 3.28707 17.7173 3.35609C17.815 3.4251 17.8891 3.52269 17.9292 3.63545Z"
-                          fill="#434F5A"
-                        />
-                      </svg>
-                      Guides
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      <svg
-                        width={24}
-                        height={24}
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M18.2364 5H5.99976V19H18.2364V5Z"
-                          stroke="#434F5A"
-                          strokeWidth="1.5"
-                          strokeLinejoin="round"
-                        />
-                        <path
-                          d="M8.64893 12.1315H15.4005"
-                          stroke="#434F5A"
-                          strokeWidth="1.5"
-                          strokeMiterlimit={10}
-                        />
-                        <path
-                          d="M8.64893 15.2604H15.4005"
-                          stroke="#434F5A"
-                          strokeWidth="1.5"
-                          strokeMiterlimit={10}
-                        />
-                        <path
-                          d="M11.4548 7.55139H8.64893V10.3572H11.4548V7.55139Z"
-                          fill="#434F5A"
-                        />
-                        <path
-                          d="M12.478 8.70374H15.4008"
-                          stroke="#434F5A"
-                          strokeWidth="1.5"
-                          strokeMiterlimit={10}
-                        />
-                      </svg>
-                      Article
-                    </a>
-                  </li>
-                </ul>
+                {first.reso && (
+                  <ul className="dropdown-menu" style={{ display: "block" }}>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        <svg
+                          width={24}
+                          height={24}
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M17.6725 6H5.32847C5.14733 6 5.00049 6.14684 5.00049 6.32798V18.672C5.00049 18.8532 5.14733 19 5.32847 19H17.6725C17.8536 19 18.0005 18.8532 18.0005 18.672V6.32798C18.0005 6.14684 17.8536 6 17.6725 6Z"
+                            stroke="#434F5A"
+                            strokeWidth="1.5"
+                            strokeMiterlimit={10}
+                          />
+                          <path
+                            d="M18.0003 13.0176H14.1638C13.9531 13.5591 13.5888 14.0273 13.1156 14.3645C12.6424 14.7018 12.0809 14.8934 11.5003 14.9158C10.9196 14.8934 10.3582 14.7018 9.88498 14.3645C9.41176 14.0273 9.04732 13.5591 8.83662 13.0176H5.00024"
+                            stroke="#434F5A"
+                            strokeWidth="1.5"
+                            strokeMiterlimit={10}
+                          />
+                        </svg>
+                        All Resources
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        <svg
+                          width={26}
+                          height={26}
+                          viewBox="0 0 26 26"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M18.8205 12.1708V20.5829H5.41675V5.25165H12.5253"
+                            stroke="#434F5A"
+                            strokeWidth="1.5"
+                            strokeLinejoin="round"
+                          />
+                          <path
+                            d="M8.32495 13.0613H15.712"
+                            stroke="#434F5A"
+                            strokeWidth="1.5"
+                            strokeMiterlimit={10}
+                          />
+                          <path
+                            d="M8.32495 16.4933H15.712"
+                            stroke="#434F5A"
+                            strokeWidth="1.5"
+                            strokeMiterlimit={10}
+                          />
+                          <path
+                            d="M8.32495 9.30566H12.4029"
+                            stroke="#434F5A"
+                            strokeWidth="1.5"
+                            strokeMiterlimit={10}
+                          />
+                          <path
+                            d="M17.9292 3.63545L18.4082 5.08368C18.442 5.1994 18.5124 5.30106 18.6088 5.37338C18.7053 5.44571 18.8225 5.48491 18.943 5.48491H20.4694C20.5917 5.48231 20.7116 5.51926 20.8112 5.59018C20.9108 5.6611 20.9849 5.76215 21.0225 5.87852C21.06 5.99489 21.059 6.1202 21.0196 6.23596C20.9802 6.35171 20.9045 6.45171 20.8038 6.52103L19.5782 7.41244C19.4793 7.4841 19.4055 7.58527 19.3675 7.70132C19.3295 7.81738 19.3291 7.94248 19.3664 8.05876L19.8344 9.50726C19.873 9.62223 19.8738 9.7465 19.8367 9.86197C19.7996 9.97745 19.7266 10.078 19.6282 10.149C19.5299 10.2199 19.4114 10.2576 19.2901 10.2564C19.1688 10.2552 19.0511 10.2151 18.9542 10.1422L17.7286 9.25075C17.6291 9.18074 17.5105 9.1433 17.3888 9.1433C17.2672 9.1433 17.1484 9.18074 17.0489 9.25075L15.8233 10.1422C15.7252 10.2118 15.6079 10.2492 15.4875 10.2491C15.3672 10.2489 15.2499 10.2114 15.1519 10.1416C15.0539 10.0718 14.9801 9.97321 14.9406 9.85953C14.9012 9.74584 14.8982 9.62275 14.9319 9.50726L15.4111 8.05876C15.4484 7.94248 15.448 7.81738 15.41 7.70132C15.372 7.58527 15.2982 7.4841 15.1993 7.41244L13.9626 6.52103C13.8682 6.4474 13.7989 6.34651 13.7643 6.23188C13.7297 6.11724 13.7313 5.99458 13.7692 5.88097C13.8071 5.76736 13.8792 5.66818 13.9757 5.59725C14.0721 5.52633 14.1883 5.48715 14.3081 5.48491H15.8233C15.9449 5.48379 16.0631 5.44455 16.1611 5.37257C16.259 5.30058 16.3319 5.19937 16.3693 5.08368L16.8372 3.63545C16.8772 3.52269 16.9513 3.4251 17.0491 3.35609C17.1468 3.28707 17.2636 3.25 17.3832 3.25C17.5029 3.25 17.6195 3.28707 17.7173 3.35609C17.815 3.4251 17.8891 3.52269 17.9292 3.63545Z"
+                            fill="#434F5A"
+                          />
+                        </svg>
+                        Guides
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        <svg
+                          width={24}
+                          height={24}
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M18.2364 5H5.99976V19H18.2364V5Z"
+                            stroke="#434F5A"
+                            strokeWidth="1.5"
+                            strokeLinejoin="round"
+                          />
+                          <path
+                            d="M8.64893 12.1315H15.4005"
+                            stroke="#434F5A"
+                            strokeWidth="1.5"
+                            strokeMiterlimit={10}
+                          />
+                          <path
+                            d="M8.64893 15.2604H15.4005"
+                            stroke="#434F5A"
+                            strokeWidth="1.5"
+                            strokeMiterlimit={10}
+                          />
+                          <path
+                            d="M11.4548 7.55139H8.64893V10.3572H11.4548V7.55139Z"
+                            fill="#434F5A"
+                          />
+                          <path
+                            d="M12.478 8.70374H15.4008"
+                            stroke="#434F5A"
+                            strokeWidth="1.5"
+                            strokeMiterlimit={10}
+                          />
+                        </svg>
+                        Article
+                      </a>
+                    </li>
+                  </ul>
+                )}
               </li>
             </ul>
             {/* <form>
@@ -261,8 +270,18 @@ export default function Navbar() {
           aria-label="Search"
         />
       </form> */}
-            <div className="dropdown" id="sideIn">
-              <a href="#">
+            <div
+              className="dropdown"
+              id="sideIn"
+              onMouseEnter={() => setfirst({ logM: true })}
+              onMouseLeave={() => setfirst({ logM: false })}
+            >
+              <a
+                href="#"
+                id="dropdown05"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 <svg
                   width={13}
                   height={18}
@@ -310,47 +329,44 @@ export default function Navbar() {
                 </svg>
                 Claim, edit, renew &amp; more
               </a>
-              <a id="Inbtn" data-bs-toggle="dropdown" aria-expanded="false">
+              {first.logM && (
+                <ul
+                  className="dropdown-menu"
+                  style={{ display: "block", top: "2.3rem" }}
+                >
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      <img src="https://myaccount.ackoassets.com/static/images/header-icons/manage-policy-v2.svg" />
+                      manage policy
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      <img src="https://myaccount.ackoassets.com/static/images/header-icons/raise-a-claim-v2.svg" />
+                      Raise a claim
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      <img src="https://myaccount.ackoassets.com/static/images/header-icons/renew-policy-v2.svg" />
+                      Renew policy
+                    </a>
+                  </li>
+                </ul>
+              )}
+              <a class="Inbtn" href="#">
                 login
               </a>
-              <ul className="dropdown-menu" aria-labelledby="dropdown04">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    <img
-                      src="https://myaccount.ackoassets.com/static/images/header-icons/manage-policy-v2.svg"
-                      className="sc-lhVmIH fxpfA"
-                    />
-                    manage policy
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    <img src="https://myaccount.ackoassets.com/static/images/header-icons/raise-a-claim-v2.svg" />
-                    Raise a claim
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    <img
-                      src="https://myaccount.ackoassets.com/static/images/header-icons/renew-policy-v2.svg"
-                      className="sc-lhVmIH fxpfA"
-                    />
-                    Renew policy
-                  </a>
-                </li>
-              </ul>
             </div>
-            <a
-              className="sc-iQKALj hWOVmJ"
-              style={{ margin: "0px 10px 0px 0px" }}
-            >
-              Help
-            </a>
+            <a style={{ margin: "0px 10px 0px 0px" }}>Help</a>
           </div>
         </div>
       </nav>
       {first.pro && (
-        <div className="mainMenuBox arrow-top">
+        <div
+          className="mainMenuBox arrow-top"
+          onMouseLeave={() => setfirst({ pro: false })}
+        >
           <div className="subMenu-Box">
             <div className="smallBoxesUnderSub">
               <div className="aLinkUnderMenu">
@@ -738,7 +754,7 @@ export default function Navbar() {
                 <img
                   src="https://myaccount.ackoassets.com/static/images/header-icons/cancel-grey-v2.svg"
                   alt="cancel"
-                  onClick={() => setfirst({ ...first, mMenu: !first.mMenu })}
+                  onClick={() => setfirst({ mMenu: false })}
                 />
               </div>
               <p className="mManuManageP">Manage your policy</p>
@@ -811,7 +827,7 @@ export default function Navbar() {
                     margin: "0px 0px 10px",
                     fontWeight: 600,
                     fontSize: 14,
-                    lineHeight: 17,
+                    lineHeight: "25px",
                     color: "rgb(138, 153, 169)",
                   }}
                 >
@@ -986,7 +1002,7 @@ export default function Navbar() {
                     margin: "0px 0px 10px",
                     fontWeight: 600,
                     fontSize: 14,
-                    lineHeight: 17,
+                    lineHeight: "25px",
                     color: "rgb(138, 153, 169)",
                   }}
                 >
