ae7ae1d77a43bcfd31fa7e45b62a6f95022c97b3
vinitkr567 <vinitkr567@gmail.com>
2022-03-30 09:58:45 +0530

    vinit Branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Router from "./Components/Routers/Router";
-
+import footer from "./Components/homepage/footer";
 function App() {
   return <Router />;
+<footer />
 }
 
 export default App;
