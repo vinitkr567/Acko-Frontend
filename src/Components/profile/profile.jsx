39ae49a6ab7b7eca627c9b9d31d3e37a4577c243
vinitkr567 <vinitkr567@gmail.com>
2022-04-01 09:54:47 +0530

    change

--- src/Components/profile/profile.jsx
+++ src/Components/profile/profile.jsx
@@ -1,22 +1,23 @@
+import pen from "./pen.svg"
 const profile = () => {
   return (
    
-    <div style={{ border: "1px solid red", width: "72%", height: "300px" }}>
+    <div style={{ boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px", width: "72%", height: "300px" }}>
     <h2>Personal details</h2>
     <div style={{borderBottom: "2px solid green", height: "30px", width: "80%", margin: "auto", display: "flex"}}>
         <div style={{width: "75%", marginLeft: "5%"}}>Name</div>
         <div className="name" style={{width: "10%", border: "1px", height: "auto"}}> vinit</div>
-        <div style={{width: "5%", border: "1px", height: "auto"}}><img src={} style={{ height: "12px", width: "12px" }}></div>
+        <div style={{width: "5%", border: "1px", height: "auto"}}><img src={pen} style={{ height: "12px", width: "12px" }}></div>
     </div>
     <div style={{borderBottom: "2px solid green", height: "30px", width: "80%", margin: "auto", display: "flex"}}>
-        <div style={{width: "75%", marginLeft: "5%"}}>Name</div>
+        <div style={{width: "75%", marginLeft: "5%"}}>Mobile No</div>
         <div className="name" style={{width: "10%", border: "1px", height: "auto"}}> vinit</div>
-        <div style={{width: "5%", border: "1px", height: "auto"}}><img src={} style={{ height: "12px", width: "12px" }}></div>
+        <div style={{width: "5%", border: "1px", height: "auto"}}><img src={pen} style={{ height: "12px", width: "12px" }}></div>
     </div>
     <div style={{borderBottom: "2px solid green", height: "30px", width: "80%", margin: "auto", display: "flex"}}>
-        <div style={{width: "75%", marginLeft: "5%"}}>Name</div>
+        <div style={{width: "75%", marginLeft: "5%"}}>Email</div>
         <div className="name" style={{width: "10%", border: "1px", height: "auto"}}> vinit</div>
-        <div style={{width: "5%", border: "1px", height: "auto"}}><img src={} style={{ height: "12px", width: "12px" }}></div>
+        <div style={{width: "5%", border: "1px", height: "auto"}}><img src={pen} style={{ height: "12px", width: "12px" }}></div>
     </div>
     
     </div>
